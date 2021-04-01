3f885b20bafe65857ca21c4865baa254a7d5a8dc
Mateus Ferro da Silva <mferrosilva14@gmail.com>
2021-03-31 22:50:34 -0300

    setted public files and routes in server file

--- src/server.js
+++ src/server.js
@@ -1,13 +1,14 @@
 const express = require("express")
 const server = express()
+const routes = require("./routes")
 
 // habilitar arquivos statics
 // adicionar configurações ao servidor, não configurar rotas
 server.use(express.static("public"))
-// request, response
-server.get('/', (request, response) => {
-    return response.sendFile(__dirname + "/views/index.html")
-})
+
+// routes
+server.use(routes)
+
 
 // Estabeleceu a conexão do servidor na porta 3000
 server.listen(3000, () => console.log('rodando'))
