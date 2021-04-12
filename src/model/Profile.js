18cb4faa9a1a838595b3c26cfc6b9f4e83dca262
Mateus Ferro da Silva <mferrosilva14@gmail.com>
2021-04-12 12:05:50 -0300

    changed js data to db data

--- src/model/Profile.js
+++ src/model/Profile.js
@@ -1,16 +1,22 @@
-let data = {
-    name: "Mateus",
-    avatar: "https://github.com/mateusferrodasilva.png",
-    "monthly-budget": 3000,
-    "hours-per-day": 5,
-    "days-per-week": 5,
-    "vacation-per-year": 4,
-    "value-hour": 75
-}
+const Database = require('../db/config')
 
 module.exports = {
-    get() {
-        return data
+    async get() {
+        const db = await Database()
+
+        const data = await db.get(`SELECT * FROM profile`)
+
+        await db.close()
+
+        return {
+            name: data.name,
+            avatar: data.avatar,
+            "monthly-budget": data.monthly_budget,
+            "hours-per-day": data.hours_per_day,
+            "days-per-week": data.days_per_week,
+            "vacation-per-year": data.vacation_per_year,
+            "value-hour": data.value_hour
+        }
     },
     update(newProfileData){
         data = newProfileData
