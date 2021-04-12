968d50d27cc76bee1b02a49f7a01e4ae3a37cf39
Mateus Ferro da Silva <mferrosilva14@gmail.com>
2021-04-12 14:40:40 -0300

    corrected NaN message on job edit and update method

--- src/controllers/JobController.js
+++ src/controllers/JobController.js
@@ -1,5 +1,6 @@
 const Job = require('../model/Job')
 const JobUtils = require('../utils/JobUtils')
+const Profile = require('../model/Profile')
 
 module.exports = {
     create(req, res) {
@@ -25,36 +26,23 @@ module.exports = {
         if (!job) {
             return res.send('Job not found!')
         }
+        
+        const profile = await Profile.get()
 
-        job.budget = JobUtils.calculateBudget(job)
+        job.budget = JobUtils.calculateBudget(job, profile['value-hour'])
 
         return res.render("job-edit", { job })
     },
     async update(req, res) {
-        const jobs = await Job.get()
         const jobId = req.params.id
-
-        const job = jobs.find(job => Number(job.id) === Number(jobId))
-
-        if (!job) {
-            return res.send('Job not found!')
-        }
-
+        
         const updatedJob = {
-            ...job,
             name: req.body.name,
             "total-hours": req.body["total-hours"],
             "daily-hours": req.body["daily-hours"]
         }
 
-        const newJobs = jobs.map(job => {
-            if (Number(job.id) === Number(jobId)) {
-                job = updatedJob
-            }
-            return job
-        })
-
-        Job.update(newJobs)
+        await Job.update(updatedJob, jobId)
 
         res.redirect('/job/' + jobId)
     },
