0d36fa3ae53aa7e19b174d3da43e4fb1a223006c
Mateus Ferro da Silva <mferrosilva14@gmail.com>
2021-04-12 09:14:34 -0300

    configured database, exported open (function to connect with the database)

--- src/db/config.js
+++ src/db/config.js
@@ -1,3 +1,11 @@
 const sqlite3 = require('sqlite3')
 // chave importa apenas este método
 const { open } = require('sqlite')
+
+// open precisa ser colocado dentro de uma estrutura (){}
+module.exports = () => {
+    open({
+        filename: './database.sqlite',
+        driver: sqlite3.Database
+    })
+}
